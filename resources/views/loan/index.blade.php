59fae35ec3c7ac6f20cbc65f42f4e3af592a0709
谢辉生 <huisheng.xhs@alibaba-inc.com>
2018-03-02 17:59:57 +0800

    add delete notice

--- resources/views/loan/index.blade.php
+++ resources/views/loan/index.blade.php
@@ -55,7 +55,7 @@
                     <p>配偶: {{ $loan->spouse }}({{ $loan->spouse_id_no }})</p>
                     <p>担保人: {{ $loan->cautioner }}</p>
                     <p>金额: {{ $loan->amount }}元</p>
-                    <p>操作: <a class="btn btn-primary" href="{{ route('loan.repay', $loan->id) }}">增加利息</a>&nbsp;&nbsp;<a class="btn btn-danger">删除</a></p>
+                    <p>操作: <a class="btn btn-primary" href="{{ route('loan.repay', $loan->id) }}">增加利息</a>&nbsp;&nbsp;<a class="btn btn-danger" data-toggle="modal" data-target="#modal-delete">删除</a></p>
                   </a>
                   </td>
                 </tr>
@@ -69,6 +69,35 @@
           @endif
         </div>
       </div>
+    </div>
+  </div>
+  {{-- 确认删除 --}}
+  <div class="modal fade" id="modal-delete" tabIndex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">确认删除</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="关闭">
+              <i class="fa fa-times" aria-hidden="true"></i> 
+              </button>
+            </div>
+            <div class="modal-body">
+              <p class="lead">
+                <i class="fa fa-question-circle fa-lg"></i>
+                确认删除该借款信息？
+              </p>
+            </div>
+            <div class="modal-footer">
+              <form method="POST" action="{{-- route('admin.photo.delete', $photo->id) --}}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="id" value="">
+                <button type="button" class="btn btn-default" data-dismiss="modal">否</button>
+                <button type="submit" class="btn btn-danger">
+                  <i class="fa fa-times-circle"></i> 是
+                </button>
+              </form>
+            </div>
+        </div>
     </div>
   </div>
 </div>
